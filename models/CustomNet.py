57297c29b01386b0f1eda2a4164c536ad2979220
Tom Bordin <tom.bordin@ens-rennes.fr>
2020-11-26 14:54:16 +0100

    Fixing jumping knowledge in encoder to bring back permutation equivariance.

--- models/CustomNet.py
+++ models/CustomNet.py
@@ -10,8 +10,6 @@ class CustomEncoder(nn.Module):
     def __init__(self, cfg: Configuration, net: NetworkCfg):
         super(CustomEncoder, self).__init__()
         self.encoder_layers = nn.ModuleList()
-        self.linear_o = []
-        self.linear_z = []
         self.residuals = cfg.residuals
         self.latent_dim = cfg.latent_dimension
         for layer, dim in zip(net.encoder_layer, net.encoder_dim):
@@ -24,22 +22,28 @@ class CustomEncoder(nn.Module):
             if layer == "MLP":
                 self.encoder_layers.append(MLP(dim[0], dim[-1], dim[1], len(dim)-2))
             # Linear layer to update z and cast to the right dimension
-            self.linear_z.append(nn.Linear(dim[-1] * cfg.set_n_points, cfg.latent_dimension))
-            self.linear_o.append(nn.Linear(cfg.latent_dimension, dim[-1] * cfg.set_n_points))
         # Layer to learn the mean
         self.mu_layer = nn.Linear(cfg.latent_dimension, cfg.latent_dimension)
         # Layer to learn the log_var
         self.log_var_layer = nn.Linear(cfg.latent_dimension, cfg.latent_dimension)
 
     def forward(self, x: torch.Tensor) -> [torch.Tensor, torch.Tensor]:
+        # z dim is [b, latent_dimension]
         z = torch.zeros(x.size(0), self.latent_dim).float()
-        for layer, lino, linz in zip(self.encoder_layers, self.linear_o, self.linear_z):
+        layer_nb = 0
+        for layer in self.encoder_layers:
+            # out dim is [bs, n, d]
             out = layer(x)
-            if self.residuals:
-                x = out + lino(z).view_as(out)
+            if self.residuals and layer_nb:
+                # x dim is [bs, n, d]
+                x = out + x
             else:
                 x = out
-            z = z + linz(out.view(out.size(0), -1))
+            layer_nb += 1
+            # pools dim are [bs, d]
+            avg_pool = nn.AvgPool1d(x.size(1))(x.transpose(1, 2)).transpose(1, 2).squeeze()
+            max_pool = nn.MaxPool1d(x.size(1))(x.transpose(1, 2)).transpose(1, 2).squeeze()
+            z = z + torch.cat((avg_pool, max_pool), dim=1)
         return self.mu_layer(z), self.log_var_layer(z)
 
 
@@ -48,9 +52,9 @@ class CustomDecoder(nn.Module):
         super(CustomDecoder, self).__init__()
         self.initial_set = nn.Parameter(torch.randn(cfg.set_n_points, cfg.set_n_feature).float(), requires_grad=True)
         self.size = cfg.set_n_points
-        self.linear_z = []
-        self.linear_o = []
-        self.linear_s = []
+        self.mlp_z = []
+        self.mlp_o = []
+        self.mlp_s = []
         self.decoder_layers = nn.ModuleList()
         self.residuals = cfg.residuals
         for layer, dim in zip(net.decoder_layer, net.decoder_dim):
@@ -63,21 +67,28 @@ class CustomDecoder(nn.Module):
             if layer == "MLP":
                 self.decoder_layers.append(MLP(dim[0], dim[-1], dim[1], len(dim)))
             # Linear layer to update z and s and cast to the right dimension
-            self.linear_z.append(nn.Linear(dim[-1] * cfg.set_n_points, cfg.latent_dimension))
-            self.linear_s.append(nn.Linear(cfg.latent_dimension, cfg.set_n_points * cfg.set_n_feature))
-            self.linear_o.append(nn.Linear(cfg.set_n_feature, dim[-1]))
-        self.mlp_z = MLP(cfg.latent_dimension, cfg.set_n_points * cfg.set_n_feature, net.decoder_dim[0][-1], 2)
+            self.mlp_z.append(MLP(dim[-1] * cfg.set_n_points, cfg.latent_dimension, 32, 2))
+            self.mlp_s.append(MLP(cfg.latent_dimension, cfg.set_n_points * cfg.set_n_feature, 32, 2))
+            self.mlp_o.append(MLP(cfg.set_n_feature, dim[-1], 32, 2))
+        self.ml_z = MLP(cfg.latent_dimension, cfg.set_n_points * cfg.set_n_feature, net.decoder_dim[0][-1], 2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        z = self.mlp_z(x).view(x.size(0), self.size, -1)
+        # z dim is [bs, latent_dim]
+        z = self.ml_z(x).view(x.size(0), self.size, -1)
         out = z + self.initial_set.repeat((x.size(0), 1, 1))
         z = x
         s = out
-        for layer, linz, lins, lino in zip(self.decoder_layers, self.linear_z, self.linear_s, self.linear_o):
+        for layer, mlpz, mlps, mlpo in zip(self.decoder_layers, self.mlp_z, self.mlp_s, self.mlp_o):
+            # out dim is [bs, n, d]
             out_l = layer(out)
-            z = z + linz(out_l.view(out.size(0), -1))
-            out = out_l + (lino(s) if self.residuals else torch.zeros_like(out_l).float())
-            s = s + lins(z).view_as(s)
+            out = out_l + (out if self.residuals else torch.zeros_like(out_l).float())
+            # pools dim are [bs, d]
+            max_pool = nn.MaxPool1d(out_l.size(1))(x.transpose(1, 2)).transpose(1, 2).squeeze()
+            avg_pool = nn.AvgPool1d(out_l.size(1))(x.transpose(1, 2)).transpose(1, 2).squeeze()
+            # z dim is [bs ,d]
+            z = z + torch.cat((avg_pool, max_pool), dim=1)
+            # ! update of s not good yet !
+            s = s + mlps(z).view_as(s)
         return s
 
 
